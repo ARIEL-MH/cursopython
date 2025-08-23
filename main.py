4c147c57cbe8b4eb15f3f55b14e3eff78db0a824
danny <adrielmon4@gmail.com>
2025-08-23 16:43:33 +0000

    Crear Variables

---
+++ main.py
@@ -0,0 +1,27 @@
+# Esto en un comentario
+
+print('Hola Mundo, desde un script en Python.')
+'''
+    Esto es un comentario de mas líneas
+    Dejar anotaciones para los desarrolladores
+'''
+"""
+    Esto es un comentario con triples comillas
+"""
+# Variables
+
+"""
+Python es un lenguaje de tipado dianmico
+<variable> = <valor>
+"""
+
+name = 'Eduardo'
+age = 10
+
+print(age)
+# Conocer el tipo de la variable
+print(type(age))
+
+# Covencion para variables con snake_case
+es_adminstrado = False
+print(es_adminstrado)
