ed98cda661a4316c4ab7e1364faf91d7aa68e6e0
danny <adrielmon4@gmail.com>
2025-08-24 15:18:43 +0000

    Indices

--- listas.py
+++ listas.py
@@ -9,11 +9,32 @@ trabajar con listas homogéneas
 # my_list = ["String", 10, 3.14, True, [1, 2, 3]]
 # print(type(list))
 
-# indices  0            1        2        3
-curses = ["Python", "Django", "flask", "Ruby",
-          # 4
-          "MongoDb"]  # lista de solo strings con 5 elementos
+
+# indices  0            1        2        3        4
+# indices  -5            -4        -3        -2        -1
+
+curses = ["Python", "Django", "Flask", "Ruby", "MongoDB"]
 number = [1, 2, 3, 4, 5]  # lista de solo números con 5 elementos
 
+
+# utilizando indice se puede modificar la colección o ver el valor del elemento
+# print(curses[0])  # Imprime Python
+
+# VER ELEMENTOS
+
+last_index = len(curses) - 1
+value = curses[last_index]  # ver el ultimo elemento
+
+value = curses[-3]  # ver el ultimo elemento con numeros negativos
+
+print(value)
+
+# print(curses)
+# print(number)
+
+
+# ACTUALIZAR ELEMENTOS
+curses[0] = "JavaScript"  # remplaza Python por JavaScript
+curses[-1] = "PostgreSQL"  # remplaza MongoDB por PostgreSQL
+
 print(curses)
-print(number)
