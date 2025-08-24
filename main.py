e0584e4042aee2903864165a7bc75c6484502cb0
danny <adrielmon4@gmail.com>
2025-08-24 14:42:11 +0000

    Introdución

--- main.py
+++ main.py
@@ -58,3 +58,86 @@ print(type(pi))
 is_active = False
 print(is_active)
 print(type(is_active))
+
+
+"""
+En python las contantes no existen
+CON MAYUSCULAS ES CONSTANTE
+"""
+VERSION = 3.14
+print(VERSION)
+
+
+# NUMEROS ENTEROS NUMEROS POSITIVOS O NEGATIVOS
+number = 10
+result = number / 10  # se obtiene siempre float
+result = number // 10  # se obtiene siempre resultado entero
+
+print("El resultado es:", result)
+
+# OPERADORES RELACIONALES se obtiene (True/False)
+# se usan para comparar numeros
+# ==, !=, >, <, >=, <=
+
+number_one = 10
+number_two = 20
+
+result = number_one < number_two  # False
+print("El resultado de la comparacion es:", result)
+print(type(result))
+
+# OPERADORES LÓGICOS
+# se usan para combinar expresiones booleanas
+# and, or, not
+
+number_one = 10
+number_two = 20
+# Todos los valores deben ser True para que sea verdadero en and
+# Con or por lo menos debe existir un verdadero para que sea verdadero
+# not se niega la expresion
+result = (
+    True
+    and True
+    and number_one != number_two
+    and number_one < 100
+    and number_two > 200
+)
+print(result)
+
+
+# PARA PEDIR VALORES POR TECLADO
+
+# por defecto toma el valor como string
+# full_name = input('Ingresa tu nombre: ')
+# print('Hola', full_name)
+
+
+# EJEMPLO DE PROGRAMA
+nombre_completo = input('Ingresa tu nombre: ')
+age = int(input('Ingresa tu edad: '))
+height = float(input('Ingresa tu estatura: '))
+# utilizo un operador relacional
+status = input('Eres estudiante (yes/no): ') == 'yes'
+
+print(nombre_completo)
+print(age)
+print(height)
+print(status)
+
+print(type(nombre_completo))
+print(type(age))
+print(type(height))
+print(type(status))
+
+print(
+    str("10")
+)
+
+# Multiples variables en una sola linea de codigo
+nombre_completo, age, height, status = (
+    "Eduardo",
+    10,
+    1.75,
+    True
+)
+print(nombre_completo, age, height, status)
