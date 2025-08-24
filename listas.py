b5095267eafd37be57e9ffff1eb0e7af68757804
danny <adrielmon4@gmail.com>
2025-08-24 14:51:14 +0000

    listas

---
+++ listas.py
@@ -0,0 +1,19 @@
+"""
+Estructura para crear listas
+<variable> = []
+
+hay que trabajar con listas que alamacen un solo tipo de dato
+trabajar con listas homogéneas
+"""
+
+# my_list = ["String", 10, 3.14, True, [1, 2, 3]]
+# print(type(list))
+
+# indices  0            1        2        3
+curses = ["Python", "Django", "flask", "Ruby",
+          # 4
+          "MongoDb"]  # lista de solo strings con 5 elementos
+number = [1, 2, 3, 4, 5]  # lista de solo números con 5 elementos
+
+print(curses)
+print(number)
