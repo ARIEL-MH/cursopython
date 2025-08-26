62c2d1db6bcbd9b7f7da9da28e79edbb773e8bf4
danny <adrielmon4@gmail.com>
2025-08-26 22:46:51 +0000

    Pass

---
+++ 08pass.py
@@ -0,0 +1,9 @@
+# pass
+var = None
+if var == None:
+    # podemos usar cualquier tipo de objecto para crear ese bloque vacio
+    ()  # también se puede usar
+    'Bloque en construcción !!'
+    ...  # también se puede usar
+    pass  # nos permite crear bloques vacíos
+    # no ejecutan ninguna instrucción
